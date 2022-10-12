263da5a96ec7a74ddc3b9668afffde6bd447a824
timothyshen <timothyshen1@hotmail.com>
2022-10-12 22:45:34 +0100

    -created request ticket call and get journey call

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TestModule } from './test/test.module';
+import { TicketsModule } from './tickets/tickets.module';
+import { PurchaseHistoryModule } from './purchase-history/purchase-history.module';
 
 @Module({
-  imports: [],
+  imports: [TestModule, TicketsModule, PurchaseHistoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
